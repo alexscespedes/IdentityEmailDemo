8697bf0a726da9b88bb33598c17f294bec78b448
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-10-24 07:01:16 -0400

    Update appsettings + program + packages

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 using IdentityEmailDemo.Data;
 using IdentityEmailDemo.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 {
@@ -19,6 +21,8 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 })
 .AddEntityFrameworkStores<AppDbContext>()
 .AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
 
 builder.Services.AddScoped<IEmailService, EmailService>();
 
